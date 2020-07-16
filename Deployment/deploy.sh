cd2f8bfaca72348a8ef83b25c2641a04251bce29
Logan Cormier <logan1189@gmail.com>
2020-07-16 18:16:54 -0400

    to run call local run from Chart file: ./Deployment/localrun.sh

--- Deployment/deploy.sh
+++
@@ -1,7 +0,0 @@
-cd ../Server/node
-
-#FEATURE: add docker deploy and server deployment options.
-
-#TODO: check for npm
-npm install
-npm start
---
+++ Deployment/localRun.sh
@@ -0,0 +1,9 @@
+# To be called from the main directory. 
+#TODO: add a path check so it can be run from anywhere. 
+cd Server/node
+
+#FEATURE: add docker deploy and server deployment options.
+
+#TODO: check for npm
+npm install
+npm start
