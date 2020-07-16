b4b391cdf0d9de63676916fd7c4a0c1cb2089ed9
Logan Cormier <logan1189@gmail.com>
2020-07-16 19:19:05 -0400

    Added client routes

--- Server/node/app.js
+++ Server/node/app.js
@@ -4,4 +4,9 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.use(express.static('../../Client/html'))
+app.use(express.static('../../Client/css'))
+app.use(express.static('../../Client/script'))
+app.use(express.static('../../Client/img'))
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
