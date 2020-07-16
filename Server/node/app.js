12c6f5a5b51673a2aa3aeb9dc511b2ceb8b94943
Logan Cormier <logan1189@gmail.com>
2020-07-16 19:26:20 -0400

    Added path.join to resolve relative path

--- Server/node/app.js
+++ Server/node/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express();
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.use(express.static('../../Client/html'))
-app.use(express.static('../../Client/css'))
-app.use(express.static('../../Client/script'))
-app.use(express.static('../../Client/img'))
+app.use(express.static(path.join(__dirname, '../../Client/html')))
+app.use(express.static(path.join(__dirname, '../../Client/css')))
+app.use(express.static(path.join(__dirname, '../../Client/script')))
+app.use(express.static(path.join(__dirname, '../../Client/img')))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
