994ef65c2b033b744585bf28955b2ece8dfac203
Logan Cormier <logan1189@gmail.com>
2020-07-30 03:43:37 -0400

    Redirect on / to main page

--- Server/node/app.js
+++ Server/node/app.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.redirect('/main.html'));
 
-app.use(express.static(path.join(__dirname, '../../Client/html')))
-app.use(express.static(path.join(__dirname, '../../Client/css')))
-app.use(express.static(path.join(__dirname, '../../Client/script')))
-app.use(express.static(path.join(__dirname, '../../Client/img')))
+app.use(express.static(path.join(__dirname, '../../Client/html')));
+app.use(express.static(path.join(__dirname, '../../Client/css')));
+app.use(express.static(path.join(__dirname, '../../Client/script')));
+app.use(express.static(path.join(__dirname, '../../Client/img')));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
