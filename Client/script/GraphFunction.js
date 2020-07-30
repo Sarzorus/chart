3766322d2fa75321e2bfd6d7504f4a605c2343c2
Logan Cormier <logan1189@gmail.com>
2020-07-30 03:15:44 -0400

    Functionality added to graph a simple mathmatical function given many details.

---
+++ Client/script/GraphFunction.js
@@ -0,0 +1,49 @@
+class GraphFunction{
+
+    // Creates a graph instance with bounds for calculating a function.
+    // The function needs the cbounding box given by the lower bounds and the graph size
+    // as well as a function that will return an y value for any given value of X.
+    // 
+    //
+    // lowerXBound is a numerical value representing the starting x value
+    // lowerYBound is a numerical value representing the starting y value
+    // range is a numerical value representing the height from the y value the graph renders
+    // domain is a numerical value representing the length from the x value the graph renders
+    // singleVariableFunction is a function that will return an y value for a given x value.
+    GraphFunction(lowerXBound, lowerYBound, range, domain,  singleVariableFunction){
+        //The following variables are the graphs limit and should always be numerical
+        this.lowerXBound = lowerXBound;
+        this.lowerYBound = lowerYBound;
+        this.range = range;
+        this.domain = domain;
+        this.increment = 1;
+        this.mathFunction = singleVariableFunction;
+    }
+
+    //Creates Data for the current graph based on a given increment.
+    //Returns a scatter trace ready data object for plotly graphing.
+    getPlotDataForIncrement(increment){
+        //TODO: Make mathfunction class, so the function can decide based on the type (IE.. linear) if
+        //      it needs more or less data to render properly. 
+        this.increment = increment; 
+        
+        let xData = [];
+        let yData = [];
+
+        let upperXBound = this.lowerXBound + this.domain;
+
+        //Populate x and y arrays for the scatter plot object
+        for(let x = this.lowerXBound; x < this.upperXBound; x = x + increment ){
+            xData.push(x);
+            yData.push(this.mathFunction(x));
+        }
+
+        let trace = {
+            x: xData,
+            y: yData,
+            type: 'scatter'
+          };
+
+        return trace;
+    }
+}
--- Client/script/graph.js
+++ Client/script/graph.js
@@ -1,19 +1,29 @@
 //Test graph setup.
 //Later make it update a plot ectera.
-var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
-    type: 'scatter'
-  };
-  
-  var trace2 = {
-    x: [1, 2, 3, 4],
-    y: [16, 5, 11, 9],
-    type: 'scatter'
-  };
-  
-  var data = [trace1, trace2];
-  
-  var chart = document.getElementById("chart")
+class Graph{
 
-  Plotly.newPlot(chart, data);
+  //Creates a 
+  constructor(){
+
+    //Currently initializes two traces one as example data one as an example function.
+    let graphFunction = new GraphFunction(0,0,10,10, function(x){return x});
+    let trace1 = graphFunction.getPlotDataForIncrement(0.2); 
+    
+    var trace2 = {
+      x: [1, 2, 3, 4],
+      y: [16, 5, 11, 9],
+      type: 'scatter'
+    };
+    
+    var data = [trace1, trace2];
+
+    var chart = document.getElementById("chart")
+    this.graph2D = Plotly.newPlot(chart, data);
+  }
+
+
+
+};
+
+//Note: this is currently the starting script which should be moved to a main function 
+var graph = new Graph();
