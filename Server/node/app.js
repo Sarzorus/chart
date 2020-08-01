9bb6724b3a3ff78c2c944b8f4f8e0204f0f40fa8
Logan Cormier <logan1189@gmail.com>
2020-07-31 23:13:24 -0400

    Documentation cleanup

--- Server/node/app.js
+++ Server/node/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = 3000;
 
+//Redirect routes for the base address that point to the main application
 app.get('/', (req, res) => res.redirect('/app.html'));
 app.get('', (req, res) => res.redirect('/app.html'));
 
+//Static path routes (Also known as "client assets")
 app.use(express.static(path.join(__dirname, '../../Client/html')));
 app.use(express.static(path.join(__dirname, '../../Client/css')));
 app.use(express.static(path.join(__dirname, '../../Client/script')));
 app.use(express.static(path.join(__dirname, '../../Client/img')));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`Chart server is listening at http://localhost:${port}`));
