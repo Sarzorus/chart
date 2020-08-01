747c81ff4a1719a686225edf4c2e3c7f0ad39809
Logan Cormier <logan1189@gmail.com>
2020-07-31 23:29:25 -0400

    Documentation Cleanup

--- Client/script/graph.js
+++ Client/script/graph.js
@@ -1,21 +1,23 @@
-//Test graph setup.
-//Later make it update a plot ectera.
+// Graph is a chart instance and relies on app.html's chart div being loaded in order to add the
+// appropriate GUI elements. 
 class Graph{
 
-  //Creates a graph with no parameters
+  //Creates a graph with default parameters
+  //TODO: Allow for user updates to parameters.
   constructor(){
 
-    //Currently initializes two traces one as example data one as an example function.
+    //Initialize a function example.
     let graphFunction = new GraphFunction(0,0,10,10, function(x){return x*x});
     let trace1 = graphFunction.getPlotDataForIncrement(0.2); 
     
+    //Initialize a dataset example.
     var trace2 = {
       x: [1, 2, 3, 4],
       y: [16, 5, 11, 9],
       type: 'scatter',
       name: 'Generic Dataset'
     };
-    
+
     var data = [trace1, trace2];
 
     var chart = document.getElementById("chart")
@@ -25,6 +27,3 @@ class Graph{
 
 
 };
-
-//Note: this is currently the starting script which should be moved to a main function 
-var graph = new Graph();
