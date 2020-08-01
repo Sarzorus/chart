b4c1358a2825c50cf0a13bdc325355f8492d7485
Logan Cormier <logan1189@gmail.com>
2020-07-31 23:34:06 -0400

    Documentation cleanup

--- Client/script/GraphFunction.js
+++ Client/script/GraphFunction.js
@@ -1,9 +1,8 @@
 class GraphFunction{
 
     // Creates a graph instance with bounds for calculating a function.
-    // The function needs the cbounding box given by the lower bounds and the graph size
+    // The function needs the bounding box given by the lower bounds and the graph size
     // as well as a function that will return an y value for any given value of X.
-    // 
     //
     // lowerXBound is a numerical value representing the starting x value
     // lowerYBound is a numerical value representing the starting y value
@@ -20,8 +19,10 @@ class GraphFunction{
         this.mathFunction = singleVariableFunction;
     }
 
-    //Creates Data for the current graph based on a given increment.
-    //Returns a scatter trace ready data object for plotly graphing.
+    // Creates Data for the current graph function (Graph area and mathmatical function) based on a given increment.
+    //
+    // increament is a numerical value representing at how often the function is evaluated
+    // Returns a trace, apropriatly formated for plotly. 
     getPlotDataForIncrement(increment){
         //TODO: Make mathfunction class, so the function can decide based on the type (IE.. linear) if
         //      it needs more or less data to render properly. 
@@ -32,7 +33,7 @@ class GraphFunction{
 
         let upperXBound = this.lowerXBound + this.domain;
 
-        //Populate x and y arrays for the scatter plot object
+        //Populates x and y arrays for the scatter plot trace
         for(let x = this.lowerXBound; x < upperXBound; x = x + increment ){
             xData.push(x);
             yData.push(this.mathFunction(x));
