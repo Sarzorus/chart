8f483b994f29ec5bc3f1b5d4361895f0a5de013f
Logan Cormier <logan1189@gmail.com>
2023-12-01 15:27:57 -0500

    Update install scripts

---
+++ Server/postgresql/install_node.sh
@@ -0,0 +1,7 @@
+# Install can be found at https://deb.nodesource.com/setup_21.3.0
+# You can check the latest version number at https://nodejs.org/en
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=21
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update && sudo apt-get install nodejs -y
